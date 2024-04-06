0423bd32dbe19b86888e4576f40a9cd0011ca69c
Arathi Variar <arathivariar@gmail.com>
2024-04-06 18:29:36 +0000

    Added Django Views

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["https://8000-arathivaria-djangobloga-3g52hty4coq.ws-eu110.gitpod.io",".herokuapp.com"]
 
