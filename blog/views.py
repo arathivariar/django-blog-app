f9b0ba28f3e6c8f5509f55ed18757742c62ffbe9
Arathi Variar <arathivariar@gmail.com>
2024-04-09 19:44:11 +0000

    Added functionality for POST in the contact form

--- blog/views.py
+++ blog/views.py
@@ -32,7 +32,7 @@ def post_detail(request, slug):
     comment_count = post.comments.filter(approved=True).count()
 
     if request.method == "POST":
-    comment_form = CommentForm(data=request.POST)
+        comment_form = CommentForm(data=request.POST)
     if comment_form.is_valid():
         comment = comment_form.save(commit=False)
         comment.author = request.user
